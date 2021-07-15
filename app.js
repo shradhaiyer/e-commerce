e6e4fee1fc1f269b7983bdca5eb640d563edc5e5
shradhaiyer <66390588+shradhaiyer@users.noreply.github.com>
2021-07-15 15:33:37 +0530

    Adding userSchema and linking it to /signup

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const morgan = require ('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 //import routes
 const userRoutes  = require('./routes/user');
 const app = express();
@@ -16,6 +20,11 @@ mongoose.connect(
   mongoose.connection.on('error', err => {
     console.log(`DB connection error: ${err.message}`)
   });
+
+  //middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 //routes middleware
 app.use("/api", userRoutes);
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const {sayHi} = require('../controllers/user');
+const {signUp} = require('../controllers/user');
+
 
 router.get('/', sayHi);
+router.post('/signup', signUp )
 
 module.exports = router;
