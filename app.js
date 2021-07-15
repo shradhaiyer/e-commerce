4f8773e45f75389f538a400e320e0ec2ea5f52aa
shradhaiyer <66390588+shradhaiyer@users.noreply.github.com>
2021-07-15 15:47:51 +0530

    User Validation

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const morgan = require ('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 
 //import routes
 const userRoutes  = require('./routes/user');
@@ -25,6 +26,8 @@ mongoose.connect(
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator());
+
 
 //routes middleware
 app.use("/api", userRoutes);
--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const {sayHi} = require('../controllers/user');
 const {signUp} = require('../controllers/user');
+const {userSignUpValidator} = require('../validator/index');
 
 
 router.get('/', sayHi);
-router.post('/signup', signUp )
+router.post('/signup', userSignUpValidator, signUp );
 
 module.exports = router;
